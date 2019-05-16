f729b7989582f1fcb8007299d588314c40cb9b38
PabloCaDa <pablo.cabreradarias@gmail.com>
2019-05-16 23:25:53 +0100

    Updates

---
+++ precache-manifest.3b0b7bbd87501be93481df5a6c45afaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8289dc8093d856b7a5c29507995a68b",
+    "url": "/fcc_calculator/index.html"
+  },
+  {
+    "revision": "340546dbf75cd35c1497",
+    "url": "/fcc_calculator/static/css/main.a0c0591d.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/fcc_calculator/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "340546dbf75cd35c1497",
+    "url": "/fcc_calculator/static/js/main.40a17758.chunk.js"
+  },
+  {
+    "revision": "8111e6d74a7dd095de22",
+    "url": "/fcc_calculator/static/js/runtime~main.76542a6f.js"
+  }
+]);
